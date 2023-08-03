0e25d2050606c8f87d26c2513e8e706c81ea803a
Andy <pmduck@hotmail.com>
2023-08-03 11:36:49 +0300

    api for adding vertex with edge

--- src/gremlin.ts
+++ src/gremlin.ts
@@ -14,13 +14,30 @@ const client = new gremlin.driver.Client(url)
 class GremlinQueries {
   #submitters = (query: string) => client.submit(`g.${query}`)
 
-  getNodes = () => {
+  #querify = (vertex: { [key: string]: string }) => {
+    let query = ''
+    for (const key in vertex) {
+      if (Number(vertex[key])) query += `.property('${key}', ${vertex[key]})`
+      else query += `.property('${key}', '${vertex[key]}')`
+    }
+    return query
+  }
+
+  check = (id: number) => g.V(id).hasNext()
+
+  getVertices = () => {
     const query = `V().project('id', 'name').by(id()).by('name')`
     return this.#submitters(query)
   }
 
-  getLinks = () => {
+  getEdges = () => {
     const query = `E().project('id','source','target').by(id()).by(outV().id()).by(inV().id())`
+    return this.#submitters(query)
+  }
+
+  addVertexWithNode = (id: number, vertex: { [key: string]: string }) => {
+    const addQuery = this.#querify(vertex)
+    const query = `addV()${addQuery}.addE('link').from(V(${id}))`
     return this.#submitters(query)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -22,14 +22,28 @@ const gremlin = GremlinQueries
 
 app.get('/graph', async (req: Request, res: Response) => {
   console.log('GET /graph')
-  const nodes = await gremlin.getNodes()
-  const links = await gremlin.getLinks()
+  const nodes = await gremlin.getVertices()
+  const links = await gremlin.getEdges()
 
   const result = {
     nodes: nodes._items.map((val: any) => Object.fromEntries(val)),
     links: links._items.map((val: any) => Object.fromEntries(val)),
   }
   request(res, result)
+})
+
+app.post('/vertex/edge', async (req: Request, res: Response) => {
+  console.log('POST /vertex/edge')
+  if (Number(req.query.id) && Object.keys(req.body).length > 0) {
+    const target = await gremlin.check(Number(req.query.id))
+    if (target) {
+      const result = await gremlin.addVertexWithNode(
+        Number(req.query.id),
+        req.body
+      )
+      request(res, result)
+    } else res.status(404).json({ error: 'Vertex not found.' })
+  } else res.status(400).json({ error: 'Invalid data.' })
 })
 
 app.listen(port, () => {
